5ac51fbd243a55a528d54aa7e74be31c84d5f343
“Ana <anajbritos@gmail.com>
2024-01-11 12:22:12 -0300

    Configuracao do projeto

---
+++ KingBurguer/KingBurguer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KingBurguer
+//
+//  Created by Ana Julia Brito de Souza on 11/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
